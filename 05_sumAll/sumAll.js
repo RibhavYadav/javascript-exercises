6399bdb83ce6959e12e8b8da481a2710ab7dc0df
draby2466 <ribhavyadav2466@gmail.com>
2024-12-30 08:51:45 +0530

    Exercise - 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    if (typeof end !== "number" || typeof start !== "number") {
+        return "ERROR";
+    }
+    if (end < 0 || start < 0) {
+        return "ERROR";
+    }
+    let total = 0;
+    for (let i = 1; i <= Math.max(start, end); i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
