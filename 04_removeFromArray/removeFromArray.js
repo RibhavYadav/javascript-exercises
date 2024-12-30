8967ab5983696d093625a28db3e0bf5fb2b03aa1
draby2466 <ribhavyadav2466@gmail.com>
2024-12-30 08:35:00 +0530

    Exercise - 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...args) {
+    const newArray = [];
+    for (let value of givenArray) {
+        if (!args.includes(value)) {
+            newArray.push(value);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
